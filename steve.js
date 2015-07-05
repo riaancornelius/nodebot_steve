d1ac04f067a07b103814348a0dcc2bcf9af65524
Riaan Cornelius <riaan.cornelius@entelect.co.za>
2015-07-05 20:58:27 +0000

    Minor tweaks and removed some logging

--- steve.js
+++ steve.js
@@ -22,16 +22,16 @@ board.on('ready', function() {
     var lMotor = new five.Motor({
         pins: {
             pwm: 'GPIO19',
-            dir: 'GPIO5',
-            cdir: 'GPIO6'
+            dir: 'GPIO6',
+            cdir: 'GPIO5'
         }
     });
 
     var rMotor = new five.Motor({
         pins: {
             pwm: 'GPIO12',
-            dir: 'GPIO16',
-            cdir: 'GPIO20'
+            dir: 'GPIO20',
+            cdir: 'GPIO16'
         }
     });
 
@@ -43,7 +43,7 @@ board.on('ready', function() {
     var moveSpeed = 255;
 
     function stop() {
-        console.log('All stop!');
+//        console.log('All stop!');
         lMotor.stop();
         rMotor.stop();
     }
@@ -65,22 +65,22 @@ board.on('ready', function() {
     }
 
     function turnLeft (timeout) {
-        console.log('pivoting left!');
+//        console.log('pivoting left!');
         turn (false, true, timeout);
     }
 
     function turnRight (timeout) {
-        console.log('pivoting right!');
+//        console.log('pivoting right!');
         turn (true, false, timeout);
     }
 
     function goStraight (timeout) {
-        console.log('Full speed ahead captain!');
+//        console.log('Full speed ahead captain!');
         turn (true, true, timeout);
     }
 
     function goBack (timeout) {
-        console.log('Iceberg ahead - Full reverse!');
+//        console.log('Iceberg ahead - Full reverse!');
         turn (false, false, timeout);
     }
 
@@ -100,7 +100,7 @@ board.on('ready', function() {
 
             resetButton.on("down", function() {
                 started = false;
-                console.log("Reset button pressed.");
+//                console.log("Reset button pressed.");
                 clearInterval(temporalLoop)
                 stop();
             });
@@ -110,7 +110,7 @@ board.on('ready', function() {
                 {
                     delay: 0,
                     task: function () {
-                        turnLeft(750);
+                        turnLeft(300);
                         scanSpot(function (val) {
                             scans.push({dir: 'left', val: val})
                             console.log('left: ', val);
@@ -120,8 +120,8 @@ board.on('ready', function() {
                 {
                     delay: 1500,
                     task: function () {
-                        turnRight(750)
-                        scanSpot(function (err, val) {
+                        turnRight(300)
+                        scanSpot(function (val) {
                             scans.push({dir: 'center', val: val})
                             console.log('center: ', val);
                         });
@@ -130,8 +130,8 @@ board.on('ready', function() {
                 {
                     delay: 1500,
                     task: function () {
-                        turnRight(750);
-                        scanSpot(function (err, val) {
+                        turnRight(300);
+                        scanSpot(function (val) {
                             scans.push({dir: 'right', val: val})
                             console.log('right: ', val);
                         });
@@ -140,20 +140,20 @@ board.on('ready', function() {
                 {
                     delay: 1500,
                     task: function () {
-                        var WALL_THRESHOLD = 15;
+                        var WALL_THRESHOLD = 50;
                         minVal = array.min(scans, 'val').val;
                         var maxVal = array.max(scans, 'val');
                         console.log(maxVal);
                         var direction = maxVal.val > WALL_THRESHOLD ? maxVal.dir : 'right';
                         console.log(direction);
                         if (direction === 'center') {
-                            turnLeft(750);
-                            goStraight(1500);
+                            turnLeft(300);
+                            goStraight(1200);
                         } else if (direction === 'left') {
-                            turnLeft(1500);
-                            goStraight(1500);
+                            turnLeft(600);
+                            goStraight(1200);
                         } else {
-                            goStraight(1500);
+                            goStraight(1200);
                         }
                     }
                 }
